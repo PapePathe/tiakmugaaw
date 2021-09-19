921febecab2b3da4d98363b307bf0ad5715a63f8
Pathe SENE <papa.pathe@vibes.com>
2021-09-19 18:57:58 -0500

    TMGAAW-14 Set the time zone to Africa/Dakar & autoload files in lib folder.
    Issue(s):
      https://workbookers.atlassian.net/browse/TMGAAW-14

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -29,8 +29,8 @@ module Tmg
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Africa/Dakar"
+    config.eager_load_paths << Rails.root.join("lib")
 
     config.generators.system_tests = true
     config.generators do |g|
