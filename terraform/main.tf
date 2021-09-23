871dd7d01bb3b06c2838c2242323aee8dffb7482
Pathe SENE <papa.pathe@vibes.com>
2021-09-22 19:20:30 -0500

    TMGAAW-16 Set ruby matrix in pipeline
    Story:
      https://workbookers.atlassian.net/browse/TMGAAW-16

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,45 @@
- terraform {
-   backend "remote" {
-     organization = "caakmugaaw"
-     workspaces {
-       name = "tiakmugaaw"
-     }
-   }
- }
+terraform {
+  backend "remote" {
+    organization = "caakmugaaw"
+    workspaces {
+      name = "tiakmugaaw"
+    }
+  }
+}
+
+resource "aws_apprunner_connection" "caakmugaaw" {
+  connection_name = "caakmugaaw"
+  provider_type   = "GITHUB"
+
+  tags = {
+    Name = "caakmugaaw-apprunner-connection"
+  }
+}
+
+resource "aws_apprunner_service" "caakmugaaw" {
+  service_name = "caakmugaaw"
+
+  source_configuration {
+    authentication_configuration {
+      connection_arn = aws_apprunner_connection.caakmugaaw.arn
+    }
+
+    image_repository {
+      image_configuration {
+        port = var.port
+      }
+      image_identifier      = "public.ecr.aws/caakmugaaw/caakmugaaw:latest"
+      image_repository_type = "ECR_PUBLIC"
+    }
+    auto_deployments_enabled = false
+  }
+
+  tags = {
+    Name = "caakmugaaw-apprunner-service"
+  }
+}
+
+resource "aws_apprunner_custom_domain_association" "caakmugaaw" {
+  domain_name = "cmg.sn"
+  service_arn = aws_apprunner_service.caakmugaaw.arn
+}
